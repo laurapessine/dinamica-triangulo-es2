0ac1a8132c837a3cc67bc0e48ffd98ca2e4bcc50
Laura Pessine <laurapessine@outlook.com>
2026-04-23 14:36:35 -0300

    refactor: standardize formatting in test_triangle.py

--- test_triangle.py
+++ test_triangle.py
@@ -5,38 +5,46 @@ def test_equilateral():
     t = Triangle(7, 7, 7)
     assert t.type == TriangleType.EQUILATERAL
 
+
 def test_isosceles():
-    t1 = Triangle(2,2,3)
+    t1 = Triangle(2, 2, 3)
     assert t1.type == TriangleType.ISOSCELES
-    t2 = Triangle(2,3,2)
+    t2 = Triangle(2, 3, 2)
     assert t2.type == TriangleType.ISOSCELES
-    t3 = Triangle(3,2,2)
+    t3 = Triangle(3, 2, 2)
     assert t3.type == TriangleType.ISOSCELES
 
+
 def test_escalene():
-    t = Triangle(3,4,5)
+    t = Triangle(3, 4, 5)
     assert t.type == TriangleType.SCALENE
 
+
 def test_full_zero():
-    t = Triangle(0,0,0)
+    t = Triangle(0, 0, 0)
     assert t.type == TriangleType.INVALID
+
 
 def test_negative():
-    t = Triangle(-1,3,2)
+    t = Triangle(-1, 3, 2)
     assert t.type == TriangleType.INVALID
+
 
 def test_escalene_invalid():
-    t = Triangle(1,2,3)
+    t = Triangle(1, 2, 3)
     assert t.type == TriangleType.INVALID
 
+
 def test_isosceles_invalid():
-    t = Triangle(5,5,10)
+    t = Triangle(5, 5, 10)
     assert t.type == TriangleType.INVALID
+
 
 def test_float():
     t = Triangle(2.5, 3.1, 6)
     assert t.type == TriangleType.INVALID
 
+
 def test_zero_mixed():
-    t = Triangle(5,0,3)
+    t = Triangle(5, 0, 3)
     assert t.type == TriangleType.INVALID
